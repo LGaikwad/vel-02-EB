5ed071e28b904adf1046216450cea44d02bb3004
LGaikwad <laxmangaikwad9130@gmail.com>
2023-01-08 12:49:57 +0530

    Basic Steps
    hi team,
    
    I have created framework structure go ahead and clone it and stark with your task.
    thanks.

---
+++ new_project/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Laxman
+ *
+ */
+module new_project {
+}
---
+++ new_project/src/new_project/MyClass.java
@@ -0,0 +1,14 @@
+package new_project;
+
+public class MyClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Added dependencies");
+		
+		System.out.println("Extent Report");
+
+		System.out.println("Log4J");
+
+		System.out.println("Basic stuff");
+	}
+}
